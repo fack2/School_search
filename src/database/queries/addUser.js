69870d981ad1e97cedfa0ff72384f2683f0d72e3
someyaaltous <someyaaltous1995@gmail.com>
2019-07-30 11:33:25 +0300

    delete config.env file

---
+++ src/database/queries/addUser.js
@@ -0,0 +1,17 @@
+const dbConnection = require("../db_connection.js");
+const addUser = (name,password, cb) => {
+
+  dbConnection.query(
+    "INSERT name,password INTO  user  ",
+    (err, res) => {
+
+      if (err) return cb(err);
+      cb(null, res);
+
+    }
+  );
+
+
+};
+
+module.exports = addUser;
