804365cd24a395d18088b0472390ec016d35fe83
yaqootturman <yaqoot.turman@gmail.com>
2019-07-31 10:41:35 +0300

    addUser file

--- src/database/queries/addUser.js
+++ src/database/queries/addUser.js
@@ -1,12 +1,12 @@
 const dbConnection = require("../db_connection.js");
-const addUser = (name,password, cb) => {
+const addUser = (name, password, cb) => {
 
   dbConnection.query(
     "INSERT name,password INTO  user  ",
     (err, res) => {
 
       if (err) return cb(err);
-      cb(null, res);
+      cb(null, res.rows[0]);
 
     }
   );
